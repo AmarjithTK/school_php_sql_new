42ac81a9a90b05c59df79c148f36fd4f1856693c
trinity <AmarjithTK>
2020-05-10 18:12:31 +0530

    Glitch resolved

--- update.php
+++ update.php
@@ -16,12 +16,14 @@ $cs=$_POST['cs'];
 
 
 $school= mysqli_connect("localhost","root","amar@2002","school") or die('wont work');
-$query="insert into $classname (regno,name,english,maths,physics,chemistry,sl,computer) values('.$regno.','$name','.$eng.','.$sl.','.$che.','.$phy.','.$maths.','.$cs.')";
-if($query==0){
+$query="insert into s2 (regno,name,english,maths,physics,chemistry,sl,computer) values('.$regno.','$name','.$eng.','.$maths.','.$che.','.$phy.','.$sl.','.$cs.')";
+$data_storage=mysqli_query($school,$query);
+
+if($data_storage==0){
     echo "the table does not exist or you have entered a wrong datatype";
     exit();
 }
-$data_storage=mysqli_query($school,$query);
+
 
 echo "done";
 mysqli_close($school);
